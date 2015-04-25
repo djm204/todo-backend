72fbfe11878a621e70cc223acdf184e098d2f954
Carl <carl@carlossus.com>
2015-04-25 14:29:06 +0800

    Removed dist folder. All source now compiles to ./src

---
+++ test/index.js
@@ -0,0 +1,7 @@
+var chai = require("chai");
+var expect = chai.expect;
+describe("tests", function () {
+    it("should test", function () {
+        expect(1).to.equal(1);
+    });
+});
---
+++ test/index.ts
@@ -0,0 +1,8 @@
+import chai = require("chai");
+var expect = chai.expect;
+
+describe("tests", () => {
+    it("should test", () => {
+        expect(1).to.equal(1);
+    });
+});
