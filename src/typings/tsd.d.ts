bc37ff1217d7a04dd2250b43e4a319dbe2d9fd42
Carl <carl@carlossus.com>
2015-04-25 14:04:44 +0800

    Added 'TodoItem' interface

--- src/typings/tsd.d.ts
+++ src/typings/tsd.d.ts
@@ -2,3 +2,9 @@
 /// <reference path="hapi/hapi.d.ts" />
 /// <reference path="bluebird/bluebird.d.ts" />
 /// <reference path="chalk/chalk.d.ts" />
+
+interface TodoItem {
+    id?: number;
+    message: string;
+    isDone?: number;
+}
