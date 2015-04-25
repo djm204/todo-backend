fc3ff5b2f75ff0c8dfc5795d710f3b5ce106c03b
Carl <carl@carlossus.com>
2015-04-25 14:08:03 +0800

    Adding mocha and chai to project

--- src/typings/tsd.d.ts
+++ src/typings/tsd.d.ts
@@ -2,6 +2,8 @@
 /// <reference path="hapi/hapi.d.ts" />
 /// <reference path="bluebird/bluebird.d.ts" />
 /// <reference path="chalk/chalk.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="chai/chai.d.ts" />
 
 interface TodoItem {
     id?: number;
